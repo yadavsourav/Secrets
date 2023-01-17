5bb2d7b2d74984c7166fb1cef63c93e6b4f9c4e6
Sourav Yadav <yadavsourav605@gmail.com>
2023-01-17 20:42:57 +0530

    session + passport + passportLocalMongoose

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
-const bcrypt = require("bcrypt");
-
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -19,6 +17,16 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.use(session({                                      //important to place it here
+secret: "Our little secret",
+resave: false,
+saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 main().catch(err => console.log(err));
  
 async function main() {
@@ -27,65 +35,73 @@ async function main() {
   mongoose.set('strictQuery', false);
   console.log("Connected");
 
-const userSchema = new mongoose.Schema({      // It is so beacause we are using encryption
+const userSchema = new mongoose.Schema({                          // It is so beacause we are using encryption
     email: String,
     password: String
 });
 
-
+userSchema.plugin(passportLocalMongoose);                        // for hashing and salting
 
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());                             // for authenticate
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
 });
 
 app.get("/login", function(req, res){
-    res.render("login");
+    res.render("login")
 });
 
 app.get("/register", function(req, res){
-    res.render("register");
+   res.render("register");
+});
+
+app.get("/secrets", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    } else {
+        res.redirect("/login")
+    }
+});
+
+app.get("/logout", function(req, res){
+    res.logout();
+    res.redirect("/");
 });
 
 app.post("/register", function(req, res){
-
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-        //store hash in your password DB
-
-        const newUser = new User({
-            email: req.body.username,
-            password: hash              //due to hashing of password
-        });
-    
-        newUser.save(function(err){
-            if(err){
-                console.log(err);
-            } else {
-                res.render("secrets");
-            }
-    
-        });
+    User.register({username: req.body.username}, req.body.password, function(err, user){     // it comes from passport local mongoose, instead of creating saving new user
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
     });
+   
    
 });
 
 app.post("/login", function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-    User.findOne({email: username}, function(err, foundUser){
-        if(err){
+    req.login(user, function(err){                          //It comes from passport
+        if (err){
             console.log(err);
         } else {
-            if(foundUser){
-                bcrypt.compare(password, foundUser.password, function(err, result){  // as we have already used res above
-                    if(result === true){
-                    res.render("secrets");
-                    }
-                });
-            }
+            password.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
         }
     });
 });
